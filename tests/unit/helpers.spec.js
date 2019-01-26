6efd7be7fa88c428926f5d552b1977812120a23f
viktorfa <vikfand@gmail.com>
2019-01-26 09:42:08 +0100

    Make tests for isProductUri function.

--- tests/unit/helpers.spec.js
+++ tests/unit/helpers.spec.js
@@ -1,5 +1,6 @@
 import {
-  getProductFromSearchResult
+  getProductFromSearchResult,
+  isProductUri,
 } from '@/helpers'
 
 describe('getProductFromSearchResult', () => {
@@ -39,5 +40,17 @@ describe('getProductFromSearchResult', () => {
     }
     const actual = getProductFromSearchResult(result, objects)
     expect(actual).toBeFalsy()
+  })
+})
+
+describe('isProductUri', () => {
+  test('should return true if product uri format', () => {
+    expect(isProductUri('shopgun:product:c4bfajYw')).toBeTruthy()
+    expect(isProductUri('kolonial:product:240')).toBeTruthy()
+    expect(isProductUri('meny:product:7035620032196')).toBeTruthy()
+    expect(isProductUri('europris:product:168606')).toBeTruthy()
+  })
+  test('should return false if mongodb id format', () => {
+    expect(isProductUri('5c4b1284aa871b0010bee398')).toBeFalsy()
   })
 })
