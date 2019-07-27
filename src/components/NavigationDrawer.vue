1822021e553db892b285fe0d5e5991ff15df238a
viktorfa <vikfand@gmail.com>
2019-07-27 08:36:36 +0200

    Some style changes in navigation drawer.

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,45 +1,41 @@
 <template>
-  <v-navigation-drawer v-model="showDrawer" fixed clipped class="grey lighten-4" app>
-    <v-list dense class="grey lighten-4">
-      <v-list-item>
-        <v-list-item-avatar>
-          <img src="/logo-256x256.png" />
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>
-            <router-link to="/">allematpriser.no</router-link>
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider />
-      <v-list-item>
-        <v-list-item-action>
-          <v-icon>settings</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title class="grey--text">Instillinger</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item href="https://crowdsource.allematpriser.no">
-        <v-list-item-action>
-          <v-icon>add</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title class="grey--text text--darken-4">Legg til tilbud</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-    <div>
+  <v-navigation-drawer v-model="showDrawer" fixed clipped app>
+    <div class="flex flex-col justify-between h-full">
       <v-list dense>
         <v-list-item>
-          <v-list-item-content :style="{'align-items': 'center'}">{{commitSha.substring(0, 8)}}</v-list-item-content>
+          <router-link to="/" class="w-full flex">
+            <v-list-item-avatar>
+              <img src="/logo-256x256.png" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                <span class="text-lg">allematpriser.no</span>
+              </v-list-item-title>
+            </v-list-item-content>
+          </router-link>
         </v-list-item>
+        <v-divider />
         <v-list-item>
-          <v-list-item-content
-            :style="{'align-items': 'center'}"
-          >© {{now.getUTCFullYear()}} allematpriser.no</v-list-item-content>
+          <v-list-item-action>
+            <v-icon>settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="grey--text">Instillinger</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item href="https://crowdsource.allematpriser.no">
+          <v-list-item-action>
+            <v-icon>add</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="grey--text text--darken-4">Legg til tilbud</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
+      <div class="text-center">
+        <p>{{commitSha.substring(0, 8)}}</p>
+        <p>© {{now.getUTCFullYear()}} allematpriser.no</p>
+      </div>
     </div>
   </v-navigation-drawer>
 </template>
